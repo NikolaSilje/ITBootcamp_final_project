56823d9eab84cf92770366b24874b24ce01796c7
Nikola Vasiljevic <nikolav.silje@gmail.com>
2022-06-21 20:14:01 +0200

    Update LoginTest - checkInputTypes

---  ITBootcamp_final_project/src/main/java/Tests/LoginTest.java
+++  ITBootcamp_final_project/src/main/java/Tests/LoginTest.java
@@ -1,17 +1,33 @@
 package Tests;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BasicTest {
 
     @Test(priority = 10)
-    public void visitTheLoginPage(){
+    public void visitTheLoginPage() {
         navPage.getENbutton().click();
         navPage.getLoginButton().click();
         Assert.assertTrue(driver.getCurrentUrl().contains("/login"),
                 "[Error] Page URL should contain '/login' ");
 
+    }
+
+    @Test(priority = 20)
+    public void checksInputTypes() {
+        navPage.getLoginButton().click();
+        Assert.assertEquals(
+                driver.findElement(By.id("email")).getAttribute("type"),
+                "email",
+                "[Error] Email input field attribute type should be - email"
+        );
+        Assert.assertEquals(
+                driver.findElement(By.id("password")).getAttribute("type"),
+                "password",
+                "[Error] Email input field should be email by type"
+        );
     }
 
 
