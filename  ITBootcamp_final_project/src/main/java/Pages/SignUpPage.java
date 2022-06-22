0afe89be2e1851d7c0658f20762e40be312bf7d3
Nikola Vasiljevic <nikolav.silje@gmail.com>
2022-06-22 16:25:45 +0200

    Updated SignUpPage and second SignUpTest - checkInputTypes

---  ITBootcamp_final_project/src/main/java/Pages/SignUpPage.java
+++  ITBootcamp_final_project/src/main/java/Pages/SignUpPage.java
@@ -1,6 +1,8 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SignUpPage {
 
@@ -9,4 +11,18 @@ public class SignUpPage {
     public SignUpPage(WebDriver driver) {
         this.driver = driver;
     }
+    public WebElement getPasswrodInput(){
+        return driver.findElement(By.id("password"));
+    }
+    public WebElement getEmailInput(){
+        return driver.findElement(By.id("email"));
+    }
+
+    public WebElement getConfirmPasswordInput(){
+        return driver.findElement(By.id("confirmPassword"));
+    }
+    public WebElement getNameInput(){
+        return driver.findElement(By.id("name"));
+    }
+
 }
---  ITBootcamp_final_project/src/main/java/Tests/SignUpTest.java
+++  ITBootcamp_final_project/src/main/java/Tests/SignUpTest.java
@@ -15,6 +15,19 @@ public class SignUpTest extends BasicTest {
         Assert.assertTrue(driver.getCurrentUrl().contains("/signup"),
                 "[Error] Page URL should contain '/signup' ");
 
+    }
 
+    @Test(priority = 20)
+    public void checkInputTypes(){
+        navPage.getSignUpButton().click();
+        Assert.assertEquals(signupPage.getEmailInput().getAttribute("type"),
+                "email",
+                "[Error] Email input field should be  'email' by type");
+        Assert.assertEquals(signupPage.getPasswrodInput().getAttribute("type"),
+                "password",
+                "[Error] Password input field should be 'password' by type");
+        Assert.assertEquals(signupPage.getConfirmPasswordInput().getAttribute("type"),
+                "password",
+                "[Error] Confirm password input field should be 'confirmPassword' by type");
     }
 }
