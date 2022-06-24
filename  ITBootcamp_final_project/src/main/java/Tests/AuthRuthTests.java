49fcea6cc51a20ee3736dbee26c01270e7276cd7
Nikola Vasiljevic <nikolav.silje@gmail.com>
2022-06-24 18:34:45 +0200

    Updated AuthRuthTests - with forbidsVisitsToHomeUrlIfNotAuthenticated

---
+++  ITBootcamp_final_project/src/main/java/Tests/AuthRuthTests.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRuthTests extends BasicTest {
+    @Test(priority = 10)
+    public void forbidsVisitsToHomeUrlIfNotAuthenticated() {
+        driver.get(baseUrl + "/home");
+        navPage.waitForLoginPageToLoadUp();
+        Assert.assertTrue(this.driver.getCurrentUrl().contains("/login"),
+                "Current site path do not contains /login in URL");
+    }
+    @Test(priority = 20)
+    public void ForbidsVisitsToHomeUrlIfNotAuthenticated() {
+        driver.get(baseUrl + "/home");
+        navPage.waitForLoginPageToLoadUp();
+        Assert.assertTrue(this.driver.getCurrentUrl().contains("/login"),
+                "Current site path do not contains /login in URL");
+}
